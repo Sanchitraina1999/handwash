c862e811aecefce979f607c0d02d123e87159d7a
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-05-12 13:51:12 +0530

    :fire:

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+from plyer import notification
+
+while True:
+    notification.notify(
+        title = 'Reminder to wash hands',
+        message = 'Hey There!, Wash your Hands.',
+        app_icon = '/home/sanchit199/Desktop/handwash/icon.ico', #change THIS by our current directory
+        timeout = 10
+    )
+    time.sleep(30*60)
