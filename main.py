c4af072c993981a8600fe70a2b08a76d3cf50360
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-05-12 13:44:37 +0530

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import time
 from plyer import notification
 
 while True:
-notification.notify(
-    title = 'Reminder to wash hands',
-    message = 'Hey There!, Wash your Hands.',
-    app_icon = '/home/sanchit199/Desktop/handwash/icon.ico',
-    timeout = 10
-)
+    notification.notify(
+        title = 'Reminder to wash hands',
+        message = 'Hey There!, Wash your Hands.',
+        app_icon = '/home/sanchit199/Desktop/handwash/icon.ico',
+        timeout = 10
+    )
