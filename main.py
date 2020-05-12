1a67aa6955310c5a3ec07691072c274557ae16b9
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-05-12 13:40:51 +0530

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     notification.notify(
         title = 'Reminder to wash hands',
         message = 'Hey There!, Wash your Hands.',
-        app_icon = '/icon.ico',
+        app_icon = '/home/sanchit199/Desktop/handwash/icon.ico',
         timeout = 10
     )
