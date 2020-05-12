72290a1971fc9f210cfffe0b91cc58a2950ce2c7
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-05-12 13:48:47 +0530

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ while True:
     notification.notify(
         title = 'Reminder to wash hands',
         message = 'Hey There!, Wash your Hands.',
-        app_icon = '/home/sanchit199/Desktop/handwash/icon.ico', #change THI
+        app_icon = '/home/sanchit199/Desktop/handwash/icon.ico', #change THIS by our current directory
         timeout = 10
     )
     time.sleep(30*60)
